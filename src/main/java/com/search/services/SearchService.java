fca373b764ee3a4ff5106348899c36c5de4c60b6
smuralee <suraj@surajmuraleedharan.com>
2018-10-13 15:04:27 -0700

    Added filters for the criterias

--- src/main/java/com/search/services/SearchService.java
+++ src/main/java/com/search/services/SearchService.java
@@ -1,7 +1,6 @@
 package com.search.services;
 
 import com.search.model.domain.filters.ProductIdFilter;
-import com.search.model.domain.filters.core.InCriteriaFilter;
 import com.search.model.view.SearchRequest;
 import com.search.model.view.SearchResponse;
 import com.search.persistence.entities.Product;
@@ -12,7 +11,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -23,12 +24,20 @@ public class SearchService implements Search {
 
     @Override
     public SearchResponse getSearchResults(SearchRequest searchRequest) {
+
+        // Get the criteria builder and create the root entity
         final CriteriaBuilder criteriaBuilder = this.entityManager.getCriteriaBuilder();
         final CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
         final Root<Product> root = criteriaQuery.from(Product.class);
-        InCriteriaFilter<Product> idFilter = new ProductIdFilter();
-        criteriaQuery.select(root).where(idFilter.apply(criteriaBuilder, root));
 
+        // Get all the predicates for applying the restrictions
+        List<Predicate> predicates = Arrays.asList(new ProductIdFilter().apply(criteriaBuilder, root));
+        Predicate predicate = criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+
+        // Passing the predicates
+        criteriaQuery.select(root).where(predicate);
+
+        // Query execution
         final TypedQuery<Product> query = this.entityManager.createQuery(criteriaQuery);
         final List<Product> resultList = query.getResultList();
         final SearchResponse response = new SearchResponse(resultList);
